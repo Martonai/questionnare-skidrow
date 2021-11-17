2769fb42ca1fe66ae97f0676477ecce54605f80e
GerXY-code <77322352+GerXY-code@users.noreply.github.com>
2021-11-17 17:11:44 +0100

    Solved the issues about reading
    I have solved the issues about multiline read from csv

--- Sourcecode/Questionnaire/application/views/read/Questions.php
+++ Sourcecode/Questionnaire/application/views/read/Questions.php
@@ -1,34 +1,58 @@
+<?php
+
+$csv_file_name = 'survey.csv'; 
+
+$handler = fopen($csv_file_name,'r');
+$data = fgetcsv($handler,1000,",");
+
+$all_record_arr = [];
+
+
+while(($data = fgetcsv($handler, 1000, ","))!==FALSE){
+        $all_record_arr[] = $data;
+        
+}
+//echo "<pre>";print_r($all_record_arr);
+fclose($handler);
+$counter = 1;
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
         <style>
-            td{
-                font-size:50px;
-            }
+          
         </style>
     </head>
     <body>
-     <?php
-        $questions = [];
-        $row = 1;
-        if (($handler = fopen("test.csv", "r")) !== FALSE) {
-        while (($data = fgetcsv($handler, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            $questions[$c] = $data[$c];
-        }
-    }
-        fclose($handler);
-      }
-    ?>
         
         <table>
-            <tr><?php foreach($questions as $d):?></tr>
-            <td><?=$d?></td>
-            <?php endforeach;?>
+            <h1>Survey</h1>
+            
+            <table>
+                <thead>
+               
+                <tbody>
+                   
+                    <?php foreach($all_record_arr as $rec):?>
+                    <th>Kérdés <?=$counter++?></th>
+                    
+                    
+                    <tr>
+                        
+                      <td><?=$rec[0]?></td>
+                      <td><?=$rec[1]?></td>
+                      <td><?=$rec[2]?></td>
+                      <td><?=$rec[3]?></td>
+                      
+                    </tr>
+                     
+                      
+                    <?php endforeach;?>
+                </tbody>
+                </thead>
+            </table>
         </table>
         
     </body>
