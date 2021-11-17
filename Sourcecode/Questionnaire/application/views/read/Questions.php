8527abe6a491ef27c377a5c022249289eff46aa5
csupix <marton.csabika@gmail.com>
2021-11-17 22:17:23 +0100

    New style

--- Sourcecode/Questionnaire/application/views/read/Questions.php
+++ Sourcecode/Questionnaire/application/views/read/Questions.php
@@ -21,14 +21,22 @@ $counter = 1;
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <style>
-          
-        </style>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         
-        <table>
-            <h1>Survey</h1>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<
+<div class="container-fluid bg-info">
+    <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+         <button type="button" class="btn btn-primary">Home</button>
+         <h1>Survey</h1>
+       
             
             <table>
                 <thead>
@@ -36,24 +44,52 @@ $counter = 1;
                 <tbody>
                    
                     <?php foreach($all_record_arr as $rec):?>
-                    <th>Kérdés <?=$counter++?></th>
+                    <th><?=$counter++?></th>
                     
                     
-                    <tr>
-                        
-                      <td><?=$rec[0]?></td>
-                      <td><?=$rec[1]?></td>
-                      <td><?=$rec[2]?></td>
-                      <td><?=$rec[3]?></td>
+                 
                       
-                    </tr>
-                     
-                      
-                    <?php endforeach;?>
+                   
                 </tbody>
                 </thead>
             </table>
-        </table>
+       
         
     </body>
 </html>
+            <h3><span class="label label-warning" id="qid"> <td><?=$rec[0]?></td></span></h3>
+        </div>
+        <div class="modal-body">
+            <div class="col-xs-3 col-xs-offset-5">
+               <div id="loadbar" style="display: none;">
+                  <div class="blockG" id="rotateG_01"></div>
+                  <div class="blockG" id="rotateG_02"></div>
+                  <div class="blockG" id="rotateG_03"></div>
+                  <div class="blockG" id="rotateG_04"></div>
+                  <div class="blockG" id="rotateG_05"></div>
+                  <div class="blockG" id="rotateG_06"></div>
+                  <div class="blockG" id="rotateG_07"></div>
+                  <div class="blockG" id="rotateG_08"></div>
+          
+              </div>
+          </div>
+
+          <div class="quiz" id="quiz" data-toggle="buttons">
+           <label class="element-animation1 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="1"><td><?=$rec[1]?></td></label>
+           <label class="element-animation2 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="2">  <td><?=$rec[2]?></td></label>
+           <label class="element-animation3 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="3"><td><?=$rec[3]?></td></label>
+           <label class="element-animation4 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="4"><td><?=$rec[4]?></td></label>
+           <br>
+           <button type="button" class="btn btn-primary">Submit</button>
+       </div>
+   </div>
+   <div class="modal-footer text-muted">
+    <span id="answer"></span>
+</div>
+</div>
+</div>
+</div>
+        
+    </body>
+</html>
+<?php endforeach;?>
